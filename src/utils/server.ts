868060375a66c68063ecf20b8a48bd77e5000e0e
Oabraham1 <abrahamojima2018@gmail.com>
2022-09-07 17:59:06 -0400

    Rate limiter added

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -4,6 +4,13 @@ import routes from "../routes"
 
 function createServer(){
     const app = express()
+    const RateLimit = require("express-rate-limit")
+    const limiter = new RateLimit({
+        windowMs: 1*60*1000,
+        max: 5
+    })
+
+    app.use(limiter)
 
     app.use(express.json())
 
